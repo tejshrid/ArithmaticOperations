bdc26b1ad64387d0064104d83a56d9ff55c4c2bb
Prashant <tejshrid@epicomm.net>
2019-04-16 12:23:36 +0530

    First Commit

---
+++ ArithmaticOperations/ArithmaticOperations/ArithmaticOperation.swift
@@ -0,0 +1,25 @@
+//
+//  ArithmaticOperation.swift
+//  ArithmaticOperations
+//
+//  Created by Prashant  on 16/04/19.
+//  Copyright © 2019 Prashant . All rights reserved.
+//
+
+import Foundation
+
+public class ArithmeticOperations{
+    
+    public static func add(firstNo: Int, secondNo: Int) -> Int{
+        return firstNo + secondNo
+    }
+    public static func subtract(firstNo: Int, secondNo: Int) -> Int{
+        return firstNo - secondNo
+    }
+    public static func multiply(firstNo: Int, secondNo: Int) ->Int{
+        return firstNo * secondNo
+    }
+    public static func divide(firstNo: Double, secondNo: Double) ->Double{
+        return firstNo/secondNo
+    }
+}
---
+++ ArithmaticOperations/ArithmaticOperations/ArithmaticOperations.h
@@ -0,0 +1,19 @@
+//
+//  ArithmaticOperations.h
+//  ArithmaticOperations
+//
+//  Created by Prashant  on 16/04/19.
+//  Copyright © 2019 Prashant . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArithmaticOperations.
+FOUNDATION_EXPORT double ArithmaticOperationsVersionNumber;
+
+//! Project version string for ArithmaticOperations.
+FOUNDATION_EXPORT const unsigned char ArithmaticOperationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArithmaticOperations/PublicHeader.h>
+
+
